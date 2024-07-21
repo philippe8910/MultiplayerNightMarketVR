d4838a551b7029e49c8499cc8110353e51dabe04
Zhe <56139194+philippe8910@users.noreply.github.com>
2024-07-21 21:45:54 +0800

    新增 音效

---
+++ Assets/SandBagCollisionComponent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandBagCollisionComponent : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource = null;
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        audioSource.Play();
+
+        Destroy(gameObject , 2);
+    }
+}
